526b752c52f36b798937dd65bd50072d43fe4a96
irina-cebotari <irina.cebotari0301@gmail.com>
2024-03-11 18:46:28 +0200

    last commit

--- index.js
+++ index.js
@@ -1,34 +1,40 @@
 // ex1
 
-const func = (a, b, c) => {
- return Math.sqrt(a+b+c)   
-}
+// const func = (a, b, c) => {
+//  return Math.sqrt(a+b+c)   
+// }
 
-console.log(func(5, 15, 25))
+// console.log(func(5, 15, 25))
 
 
 
 // ex2
+// Method1
+// const str = 'here we have some text written for no reason'
+// const str1 = str.replaceAll(' ', '')
 
-const str = 'here we have some text written for no reason'
-const str1 = str.replaceAll(' ', '')
+// const func1 = (str1) => {
+//     let index = Math.floor(Math.random() * str1.length)
+//     return (str1 [index])
+// }
 
-const func1 = (str1) => {
-    let index = Math.floor(Math.random() * str1.length)
-    return (str1 [index])
-}
-
-console.log(func1(str1))
+// console.log(func1(str1))
 
 
+// Method2
+// const someFunc = (string) => {
+//     return string[Math.floor(Math.random() * string.length)]
+// }
+
+// console.log(someFunc('string'))
 
 // ex3
 
-const str2 = 'i have written some text here '
-const str3 = 'but the text have no sense'
+// const str2 = 'i have written some text here '
+// const str3 = 'but the text have no sense'
 
-const func2 = (a, b) => {
-    return (a+b)
-}
+// const func2 = (a, b) => {
+//     return (a+b)
+// }
 
-console.log(func2(str2, str3))
+// console.log(func2(str2, str3))
